caf55963f93e59795a59653ba6a259ad6ca5a01a
saurluca <spuckteller@gmail.com>
2025-05-09 08:33:25 +0200

    plot num spikes

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def euler_integrate(
 
     return x
 
-
 def RC_derivative(tau, I):
     """f(x, t)"""
 
@@ -29,6 +28,7 @@ def RC_derivative(tau, I):
     return deriv
 
 
+# Plotting the trajectory of the voltage
 def plot_trajectory(
     t: np.ndarray, V: np.ndarray, ylab="", xlab="Time (ms)", title: str = ""
 ):
@@ -40,14 +40,13 @@ def plot_trajectory(
     plt.tight_layout()
 
 
+# These functions are the activation functions in relation to the voltage and based on alpha and beta
 def alpha_m(V):
-    # Avoid division by zero
     denominator = 1 - np.exp(-(V + 54) / 4)
     return 0.32 * (V + 54) / denominator
 
 
 def beta_m(V):
-    # Avoid division by zero
     denominator = np.exp((V + 27) / 5) - 1
     return 0.28 * (V + 27) / denominator
 
@@ -65,7 +64,6 @@ def beta_h(V):
 
 
 def alpha_n(V):
-    # Avoid division by zero
     denominator = 1 - np.exp(-(V + 52) / 5)
     return 0.032 * (V + 52) / denominator
 
@@ -76,44 +74,32 @@ def beta_n(V):
     return 0.5 * np.exp(V_clipped)
 
 
+# This function calculates the time constant for the gating variables
 def tau_x(V, alpha_x, beta_x):
-    # Avoid division by zero
     denominator = alpha_x(V) + beta_x(V)
     return 1 / denominator
 
 
+# This function calculates the steady-state value for the gating variables
 def x_inf(V, alpha_x, beta_x):
-    # Avoid division by zero
     denominator = alpha_x(V) + beta_x(V)
     return alpha_x(V) / denominator
 
 
+# This function calculates the derivative of the gating variables
 def x_deriv(V, x, alpha_x, beta_x):
     return -1 / tau_x(V, alpha_x, beta_x) * (x - x_inf(V, alpha_x, beta_x))
 
 
-# m: Sodium activation gate
-# h: Sodium inactivation gate
-def I_NA(cfg, m, h, V):
-    return cfg.gNa * m**3 * h * (V - cfg.ENa)
-
-
-# n: Potassium activation gate
-def I_K(cfg, n, V):
-    return cfg.gK * n**4 * (V - cfg.EK)
-
-
-def I_L(cfg, V):
-    return cfg.gL * (V - cfg.EL)
-
-
-def C_m_deriv(cfg, I_ext, V, m, h, n):
-    return I_ext - I_NA(cfg, m, h, V) - I_K(cfg, n, V) - I_L(cfg, V)
-
-
-def hh_derivatives(cfg, I):
+# This function calculates the derivatives of the whole hodgkin huxley model
+def hh_derivatives(params, I):
     def derivs(t, x):
+        # unpack the state variables
         V, m, h, n = x  # x = [V, m, h, n]
+        
+        # m: Sodium activation gate
+        # h: Sodium inactivation gate
+        # n: Potassium activation gate
 
         # Calculate the time constants for the gating variables
         tau_m = tau_x(V, alpha_m, beta_m)
@@ -126,73 +112,68 @@ def hh_derivatives(cfg, I):
         n_inf = x_inf(V, alpha_n, beta_n)
 
         # derivative of the gating variables
-        m_deriv = (m_inf - m) / tau_m
+        m_deriv = (m_inf - m) / tau_m 
         h_deriv = (h_inf - h) / tau_h
         n_deriv = (n_inf - n) / tau_n
 
         # Calculate the ionic currents
-        g_na = cfg.gNa * m**3 * h
-        g_k = cfg.gK * n**4
-        g_l = cfg.gL
+        g_na = params.gNa * m**3 * h # Sodium conductance
+        g_k = params.gK * n**4 # Potassium conductance
+        g_l = params.gL # Leak conductance
 
-        I_Na = g_na * (V - cfg.ENa)
-        I_K = g_k * (V - cfg.EK)
-        I_L = g_l * (V - cfg.EL)
+        I_Na = g_na * (V - params.ENa) # Sodium current
+        I_K = g_k * (V - params.EK) # Potassium current
+        I_L = g_l * (V - params.EL) # Leak current
 
-        v_deriv = (I(t) - I_Na - I_K - I_L) / cfg.Cm
+        # Calculate the derivative of the voltage
+        v_deriv = (I(t) - I_Na - I_K - I_L) / params.Cm
 
         return np.array([v_deriv, m_deriv, h_deriv, n_deriv])
 
     return derivs
 
-
-def simulate_hh(cfg, I, T=200.0, dt=0.025, v0=-65.0):
+# simulate the hodgkin huxley model
+def simulate_hh(params, I, T=200.0, dt=0.025, v0=-65.0):
     t = np.arange(0.0, T + dt, dt)
 
+    # Calculate the steady-state values for the gating variables
     m_inf = x_inf(v0, alpha_m, beta_m)
     h_inf = x_inf(v0, alpha_h, beta_h)
     n_inf = x_inf(v0, alpha_n, beta_n)
 
+    # Initialize the state variables
     x0 = np.array([v0, m_inf, h_inf, n_inf])
 
-    traj = euler_integrate(hh_derivatives(cfg, I), x0, t)
+    # Integrate the derivatives of the hodgkin huxley model
+    traj = euler_integrate(hh_derivatives(params, I), x0, t)
 
+    # return the trajectory of the voltage, m, h, and n as a dictionary
     return {"t": t, "V": traj[:, 0], "m": traj[:, 1], "h": traj[:, 2], "n": traj[:, 3]}
 
 
-cfg = SimpleNamespace(**{})
-
-cfg.dt = 0.1
-cfg.T = 50
-cfg.t = np.arange(0.0, cfg.T + cfg.dt, cfg.dt)
-cfg.tau = 20
+params = SimpleNamespace(**{})
 
 # These variables are the parameters for the Hodgkin-Huxley model
-cfg.Cm = 1.0  # Membrane capacitance
-cfg.gNa = 50.0  # Sodium conductance
-cfg.gK = 10.0  # Potassium conductance
-cfg.gL = 0.1  # Leak conductance
-cfg.ENa = 50.0  # Sodium reversal potential
-cfg.EK = -90.0  # Potassium reversal potential
-cfg.EL = -65.0  # Leak reversal potential
-
-# Initial conditions
-cfg.V_0 = -65.0
-cfg.m_0 = x_inf(cfg.V_0, alpha_m, beta_m)
-cfg.h_0 = x_inf(cfg.V_0, alpha_h, beta_h)
-cfg.n_0 = x_inf(cfg.V_0, alpha_n, beta_n)
-cfg.x_0 = [cfg.V_0, cfg.m_0, cfg.h_0, cfg.n_0]
+params.Cm = 1.0  # Membrane capacitance
+params.gNa = 50.0  # Sodium conductance
+params.gK = 10.0  # Potassium conductance
+params.gL = 0.1  # Leak conductance
+params.ENa = 50.0  # Sodium reversal potential
+params.EK = -90.0  # Potassium reversal potential
+params.EL = -65.0  # Leak reversal potential
 
 
+v_array = np.linspace(-100, 40, 100)
 
+tau_m_array = tau_x(v_array, alpha_m, beta_m)
+tau_n_array = tau_x(v_array, alpha_n, beta_n)
+tau_h_array = tau_x(v_array, alpha_h, beta_h)
 
+x_inf_m_array = x_inf(v_array, alpha_m, beta_m)
+x_inf_n_array = x_inf(v_array, alpha_n, beta_n)
+x_inf_h_array = x_inf(v_array, alpha_h, beta_h)
 
+plot_trajectory(v_array, tau_m_array, xlab="Voltage (mV)", ylab="Tau")
+plot_trajectory(v_array, tau_n_array, xlab="Voltage (mV)", ylab="Tau")
+plot_trajectory(v_array, tau_h_array, xlab="Voltage (mV)", ylab="Tau")
 
-"""
-So basciallt the neuron can model different activation functions based on the parameters.
-the parameters are the number of each type of ion channel ? AAAND?
-
-Is this fixed for each neuron ?
-
-
-"""
